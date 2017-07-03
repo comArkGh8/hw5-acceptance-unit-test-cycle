7f2ccc3bbf8dc01ee561374baa5e3b0b4aeec137
Saul Duphraigne <comarkgh8@gmail.com>
2017-07-03 17:21:09 +0200

    passed happy paths of part 2

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -3,7 +3,26 @@ require 'rails_helper'
 describe MoviesController do
   
   
+  describe 'Find movies by the same director' do
+    # just see if .same_director is activated
+    it 'should call Movie.same_director' do
+      expect(Movie).to receive(:same_director).with('Star Wars')
+      get :find, { title: 'Star Wars' }
+    end
 
+    # now enter info into .same_director
+    # it supposes that when apocalypse now is fed into same_director,
+    # the list including lost is given
+    # check to see if results of find are the movies
+    it 'should assign similar movies if director exists' do
+      movies = ['apocalypse now', 'lost']
+      Movie.stub(:same_director).with('apocalypse now').and_return(movies)
+      get :find, { title: 'apocalypse now' }
+      expect(assigns(:movies_with_director)).to eql(movies)
+    end
+
+
+  end
 
 end
 
